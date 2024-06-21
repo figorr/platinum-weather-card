06a5cbed3e47033ae6c55ec2b87316197efd45e6
figorr <77553975+figorr@users.noreply.github.com>
2024-06-22 00:14:17 +0200

    Add files via upload

---
+++ rollup.config.dev.js
@@ -0,0 +1,48 @@
+import resolve from 'rollup-plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
+import keysTransformer from 'ts-transformer-keys/transformer';
+import babel from 'rollup-plugin-babel';
+import serve from 'rollup-plugin-serve';
+import { terser } from 'rollup-plugin-terser';
+import json from '@rollup/plugin-json';
+import ignore from './rollup-plugins/ignore';
+import { ignoreTextfieldFiles } from './elements/ignore/textfield';
+import { ignoreSelectFiles } from './elements/ignore/select';
+import { ignoreSwitchFiles } from './elements/ignore/switch';
+
+export default {
+  input: ['src/platinum-weather-card.ts'],
+  output: {
+    dir: './dist',
+    format: 'es',
+    inlineDynamicImports: true,
+  },
+  watch: {
+    include: './src/**',
+    clearScreen: false,
+  },
+  plugins: [
+    resolve(),
+    typescript({ transformers: [service => ({
+      before: [ keysTransformer(service.getProgram()) ],
+      after: []
+    })] }),
+    json(),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+//    terser(),
+    serve({
+      contentBase: './dist',
+      host: '0.0.0.0',
+      port: 5000,
+      allowCrossOrigin: true,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+    }),
+    ignore({
+      files: [...ignoreTextfieldFiles, ...ignoreSelectFiles, ...ignoreSwitchFiles].map((file) => require.resolve(file)),
+    }),
+  ],
+};
